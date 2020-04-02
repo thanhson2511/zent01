86931e8575a596c7c17c02bc0d3076e2154b35c4
SonBui <45113894+thanhson2511@users.noreply.github.com>
2020-04-02 17:09:47 +0700

    Update post_edit_process.php

--- post_edit_process.php
+++ post_edit_process.php
@@ -3,7 +3,6 @@
 	$id = $_POST['id'];
 	require 'connect.php';
 	$query = " UPDATE posts SET title ='". $data['title'] . "',description='". $data['description'] ."',content='". $data['content'] . "' ,user_id='". $data['user_id'] ."' ,category_id='". $data['category_id'] ."' WHERE id= " . $id ;
-	die();
 
 	$status = $conn->query($query);
 	header("Location: posts.php");
